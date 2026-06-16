8c0f4ab6718d997f75f562b993844703f05b52ac
Jasper <jasper.haegeman@skynet.be>
2026-06-16 17:07:49 +0200

    add common entityConfiguration to reuse

---
+++ ToDoManager.Persistence/Common/EntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoManager.Domain.Common;
+
+namespace ToDoManager.Persistence.Common
+{
+    internal class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.ToTable(typeof(TEntity).Name);
+
+            builder.Property(p => p.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(p => p.UpdatedAt).HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(p => p.IsDeleted).HasDefaultValue(false);
+        }
+    }
+}
