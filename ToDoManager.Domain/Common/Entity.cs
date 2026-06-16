c9d8d1c1a9f280f4779e55ac52cca8c49c4bb010
Jasper <jasper.haegeman@skynet.be>
2026-06-16 12:44:44 +0200

    add domain to solution

---
+++ ToDoManager.Domain/Common/Entity.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ToDoManager.Domain.Common
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        protected Entity() { }
+
+        protected Entity(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ ToDoManager.Domain/ToDoItem/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Versioning;
+using ToDoManager.Domain.Common;
+
+namespace ToDoManager.Domain.ToDoItem;
+
+public class ToDoItem : Entity
+{
+
+    private string _title = default!;
+    public required string Title 
+    {
+        get => _title;
+        set => _title; 
+    }
+}
