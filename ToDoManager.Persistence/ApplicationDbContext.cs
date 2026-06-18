6cdbf0755f65912e467f8ba3ba5ed1906c63d99d
Jasper <jasper.haegeman@skynet.be>
2026-06-18 11:48:54 +0200

    add ApplicationDbContext for ToDoItem

---
+++ ToDoManager.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManager.Domain.ToDoItems;
+
+namespace ToDoManager.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            modelBuilder.Entity<ToDoItem>().HasQueryFilter(s => !s.IsDeleted);
+        }
+    }
+}
