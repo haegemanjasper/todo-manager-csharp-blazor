b6247501735ef9fbc03b94c664727ce69124b2d4
Jasper <jasper.haegeman@skynet.be>
2026-06-18 11:36:21 +0200

    add entity configuration for ToDoItem

---
+++ ToDoManager.Persistence/ToDoItems/ToDoItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoManager.Domain.ToDoItems;
+using ToDoManager.Persistence.Common;
+
+namespace ToDoManager.Persistence.ToDoItems
+{
+    internal class ToDoItemConfiguration : EntityConfiguration<ToDoItem>
+    {
+        public override void Configure(EntityTypeBuilder<ToDoItem> builder)
+        {
+            base.Configure(builder);
+            builder.Property(p => p.Title).HasMaxLength(250).IsRequired();
+            builder.Property(p => p.Priority).HasConversion<string>().IsRequired();
+
+        }
+    }
+}
