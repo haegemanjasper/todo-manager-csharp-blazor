61aab219993bf073208e161dee8135bbecccc454
Jasper <jasper.haegeman@skynet.be>
2026-08-02 15:19:39 +0200

    add MappingProfile to Services

---
+++ ToDoManager.Services/Common/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ToDoManager.Shared.ToDoItems;
+using ToDoManager.Domain.ToDoItems;
+
+namespace ToDoManager.Services.Common;
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ToDoItem, ToDoItemDto.Index>();
+            CreateMap<ToDoItemDto.Create, ToDoItem>();
+            CreateMap<ToDoItemDto.Edit, ToDoItem>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember is not null));
+            CreateMap<ToDoItem, ToDoItemDto.Detail>()
+                .ForMember(dest => dest.Title, t => t.MapFrom(src => src.Title));
+        }
+    }
